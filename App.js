e7c463609c193d89e7802590260a85033327a30a
Ronnie-Leon76 <ronleon76@gmail.com>
2022-02-01 22:44:04 +0300

    added icons

--- App.js
+++ App.js
@@ -8,9 +8,12 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import CategoryMealScreen from "./screens/CategoryMealScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
+import FavoriteScreen from "./screens/FavoriteScreen";
 import Color from "./colors/Color";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import CustomHeaderButton from "./components/HeaderButton";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
 const fetchFonts = () => {
   return Fonts.loadAsync({
@@ -20,6 +23,59 @@ const fetchFonts = () => {
 };
 
 const Stack = createNativeStackNavigator();
+
+function StackScreen() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{
+          title: "Categories",
+          headerStyle: { backgroundColor: Color.primaryColor },
+        }}
+      />
+      <Stack.Screen
+        name="Category Meals"
+        component={CategoryMealScreen}
+        options={({ route }) => ({
+          title: route.params.categoryTitle,
+          headerStyle: {
+            backgroundColor: { backgroundColor: Color.accentColor },
+          },
+        })}
+      />
+      <Stack.Screen
+        name="Meal Detail"
+        component={MealDetailScreen}
+        options={({ route }) => ({
+          title: route.params.mealTitle,
+          headerRight: () => (
+            <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+              <Item
+                //title="Favorite"
+                iconName="ios-star"
+                onPress={() => {
+                  console.log("Mark as favorite!");
+                }}
+              ></Item>
+            </HeaderButtons>
+          ),
+        })}
+      />
+    </Stack.Navigator>
+  );
+}
+const FavoriteStack = createNativeStackNavigator();
+function FavoriteStackScreen() {
+  return (
+    <FavoriteStack.Navigator>
+      <FavoriteStack.Screen name="Favorites" component={FavoriteScreen} />
+    </FavoriteStack.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   const [fontLoaded, setFontLoaded] = useState(false);
@@ -36,46 +92,28 @@ export default function App() {
       />
     );
   }
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Categories"
-          component={CategoriesScreen}
-          options={{
-            title: "Meals Categories",
-            headerStyle: { backgroundColor: Color.primaryColor },
-          }}
-        />
-        <Stack.Screen
-          name="Category Meals"
-          component={CategoryMealScreen}
-          options={({ route }) => ({
-            title: route.params.categoryTitle,
-            headerStyle: {
-              backgroundColor: { backgroundColor: Color.accentColor },
-            },
-          })}
-        />
-        <Stack.Screen
-          name="Meal Detail"
-          component={MealDetailScreen}
-          options={({ route }) => ({
-            title: route.params.mealTitle,
-            headerRight: () => (
-              <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
-                <Item
-                  title="Favorite"
-                  iconName="ios-star"
-                  onPress={() => {
-                    console.log("Mark as favorite!");
-                  }}
-                ></Item>
-              </HeaderButtons>
-            ),
-          })}
-        />
-      </Stack.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({}) => {
+            let iconName;
+
+            if (route.name === "Meals") {
+              iconName = "ios-restaurant";
+            } else if (route.name === "favorite") {
+              iconName = "ios-star";
+            }
+            return (
+              <Ionicons name={iconName} size={25} color={Color.primaryColor} />
+            );
+          },
+        })}
+      >
+        <Tab.Screen name="Meals" component={StackScreen} />
+        <Tab.Screen name="favorite" component={FavoriteStackScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
